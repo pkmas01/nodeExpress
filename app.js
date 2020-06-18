a52980148a8063c28c0d68602d20ac7fcfa2c33a
Samuel Pérez <samuel.perez@shopadvizor.com>
2020-06-18 15:48:10 +0200

    Servidor nodemon + eslint.

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var express = require('express');
+const express = require('express');
 
-var app = express();
+const app = express();
 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res)=>{
-    req.send('hello world');
+app.get('/', (req, res) => {
+  req.send('hello world');
 });
 
-app.listen(port, ()=>{
-    console.log("running");
+app.listen(port, () => {
+  console.log( `running ${port}`);
 });
